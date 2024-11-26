4c23623b8dcd128d04bcc2b6c0237bbcc07ff1e5
Asfand <you@example.com>
2024-11-26 17:03:51 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Icon name="bicycle-outline" size={80} />
+    </View>
+  );
+};
+
+export default App;
